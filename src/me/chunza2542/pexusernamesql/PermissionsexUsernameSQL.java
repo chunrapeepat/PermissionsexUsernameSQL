069d1c7aac805ce60b4631a2d1a27bb31eed31cb
Chun Rapeepat <chunza2542@hotmail.com>
2017-06-29 17:07:02 +0700

    Initialize Project

---
+++ src/me/chunza2542/pexusernamesql/PermissionsexUsernameSQL.java
@@ -0,0 +1,11 @@
+package me.chunza2542.pexusernamesql;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PermissionsexUsernameSQL extends JavaPlugin{
+	
+	public void onEnable(){
+		
+	}
+
+}
